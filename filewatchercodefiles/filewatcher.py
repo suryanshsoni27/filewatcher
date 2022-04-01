d8beeefa4e5fe60bc8c1576abf21999044f702b5
suryanshsoni27 <szscode2727@gmail.com>
2022-04-01 00:44:09 -0500

    work:

--- filewatchercodefiles/filewatcher.py
+++ filewatchercodefiles/filewatcher.py
@@ -1,7 +1,7 @@
+from datetime import date
 import sys 
 import time 
 import logging
-from pymysql import Date 
 import watchdog.events 
 import watchdog.observers 
 from watchdog.observers import Observer 
@@ -10,7 +10,6 @@ from watchdog.events import FileSystemEventHandler
 from dotenv import dotenv_values,load_dotenv 
 import pandas as pd
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy_utils import database_exists,create_database
 from sqlalchemy import Table, Column, Integer, String, MetaData
 from sqlalchemy import create_engine
 import os
@@ -23,39 +22,30 @@ class Handler(watchdog.events.PatternMatchingEventHandler):
         watchdog.events.PatternMatchingEventHandler.__init__(self, patterns=['*.csv','*.txt','*.pdf','*.json'], 
                                                              ignore_directories=True, case_sensitive=False)
     
-    def get_engine(self,url):
-        if not database_exists(url):
-            create_database(url)
-        self.engine = create_engine(url,pool_size=50, echo=False)
-        print(self.engine.url)
-        return self.engine
+    def get_engine(self):
+        self.engine = create_engine('sqlite:///commodity.db', echo=False)
+        self.session = self.engine.connect()
+        return self.engine,self.session
     
-    def commodityModified(self):
-        url = 'sqlite://commodityModified.db'
-        self.get_engine(url)
-        engine = self.get_engine()
-        self.session = sessionmaker(bind = engine)() 
-        return self.session
     
-    def commodity(self):
-        url = 'sqlite://commodity.db'
-        self.get_engine(url)
-        engine = self.get_engine()
-        self.session = sessionmaker(bind = engine)() 
-        return self.session
+    #how to add a model 
+    
+    
+    def commodityModifiedUpload(self,df,path):
+        sqlConnection,engine=  self.get_engine()
+        sqlite_table = "CommodityModified_" + path
+        df.to_sql(sqlite_table, sqlConnection, if_exists='fail') 
         
-       
-       
-    def get_sessions(self):
-        commoditySession = self.commodity()
-        commodityModifiedSession = self.commodityModified()
-        return commoditySession,commodityModifiedSession
         
     
+    def commodityUpload(self,df,path):
+        sqlConnection,engine=  self.get_engine()
+        sqlite_table = "Commodity_" + path
+        df.to_sql(sqlite_table, sqlConnection, if_exists='fail')  
+
+ 
     #Adds newly created file to the sql database 
     def on_created(self, event):
-        cm,mcm = self.get_sessions()
-        
         print(event.src_path)
         print("Watchdog received created event - % s." % event.src_path)
         split_tup = os.path.splitext(event.src_path)
@@ -63,40 +53,38 @@ class Handler(watchdog.events.PatternMatchingEventHandler):
             df = pd.read_csv(event.src_path)
             columns = df.columns
             print(columns)
-            df = df.astype({"Date": Date}, errors='raise')
-            df = df.astype({"Commodity": str}, errors='raise')
             pathname = event.src_path.split("/")
-            path = pathname[len(pathname)-1]
-            df = df.astype({"index":Integer})
+            path = pathname[len(pathname)-1]          
             columns = df.columns
             for i in columns[2:len(columns)]:
-                df[i] = pd.to_numeric(i)
+                df[i] = df[i].str.rstrip('%').astype('float')
             df['Date'] = pd.to_datetime(df['Date'])
-            commoiditydata = Table(columns)
-           
-            cm.add(df)
-            cm.commit()        
+            self.commodityUpload(df,path) 
            
 
-            
+    
             
     #Adds modified file to sql database
     def on_modified(self, event): 
-        cm,mcm = self.get_sessions()
         print("Watchdog received modified event - % s." % event.src_path)
         split_tup = os.path.splitext(event.src_path)
-        if split_tup[1] == ".csv":
-            
+        if split_tup[1] == ".csv":   
             df = pd.read_csv(event.src_path)
+            columns = df.columns
             pathname = event.src_path.split("/")
-            path = pathname[len(pathname)-1]
-            stagedtable = Table(df.columns)
-            mcm.add(df)
+            path = pathname[len(pathname)-1]  
+            columns = df.columns
+            for i in columns[2:len(columns)]:
+                df[i] = df[i].str.rstrip('%').astype('float')
+                
+            df['Date'] = pd.to_datetime(df['Date'])
+            self.commodityModifiedUpload(df,path) 
             
-            #load one row that is modified
-            #look into sql merge function to load only the row that has been modified
-            #create another table commodity staging table , whenever file is modifed andload it into staging tablen
-            #use sql merge to check difference between staging and file 
+            
+            # #load one row that is modified
+            # #look into sql merge function to load only the row that has been modified
+            # #create another table commodity staging table , whenever file is modifed andload it into staging tablen
+            # #use sql merge to check difference between staging and file 
             
     def on_deleted(self, event):
         print(event.src_path, event.event_type)
